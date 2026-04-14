c3cc59837c3fdfcb3480a317287125e1a97d4459
Lucifer <anuragyad1398@gmail.com>
2026-04-14 17:58:17 +0530

    fix api base url

--- frontend/src/services/baseApi.js
+++ frontend/src/services/baseApi.js
@@ -1,38 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-// const baseQuery = fetchBaseQuery({
-//     baseUrl: import.meta.env.VITE_API_BASE_URL,
-//     prepareHeaders: (headers) => {
-//         const token = localStorage.getItem("access");
-
-//         if (token) {
-//             headers.set("Authorization", `Bearer ${token}`);
-//         }
-
-//         return headers;
-//     },
-// });
-
-// export const baseApi = createApi({
-//     reducerPath: "api",
-//     baseQuery,
-//     tagTypes: ["Auth", "Dashboard"],
-//     endpoints: () => ({}),
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // ================= BASE QUERY =================
