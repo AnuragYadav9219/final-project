31103a3cfcdf5641f8821cdb61ad4179aa16f2da
Lucifer <anuragyad1398@gmail.com>
2026-04-14 18:06:21 +0530

    fix cors properly

--- core/core/settings.py
+++ core/core/settings.py
@@ -129,7 +129,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS").split(",")
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    "https://final-project-psi-pink.vercel.app",
+]
+
+origins = os.getenv("CORS_ALLOWED_ORIGINS", "")
+CORS_ALLOWED_ORIGINS = [o.strip() for o in origins.split(",") if o]
 
 
 # Static files (CSS, JavaScript, Images)
